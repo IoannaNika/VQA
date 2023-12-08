d49d80566d353ef58b86feb3e02e3e261834a30a
IoannaNika <nana11892@gmail.com>
2023-12-09 00:25:49 +0100

    fix loss cpu, gpu

--- src/vqa/loss/ContrastiveCrossEntropy.py
+++ src/vqa/loss/ContrastiveCrossEntropy.py
@@ -14,6 +14,6 @@ class ContrastiveLoss(torch.nn.Module):
         loss = bce_loss(l1_distance, label.unsqueeze(1))
         
         # if the negative class probability is greater than 0.5, then the model predicts the negative class so predicted label is 1 else 0
-        predicted_labels = torch.where(l1_distance > 0.7, torch.tensor([1]).to('cpu'), torch.tensor([0]).to('cpu'))
+        predicted_labels = torch.where(l1_distance > 0.5, torch.tensor([1]).to('gpu'), torch.tensor([0]).to('gpu'))
         # calculate accuracy
         return loss, predicted_labels
