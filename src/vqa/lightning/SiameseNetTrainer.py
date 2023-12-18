fix bugs (accuracy calculation and add supprot for amplicon reads (simulated))

src/vqa/lightning/SiameseNetTrainer.py
@@ -22,7 +22,6 @@ class SiameseNetTrainer(pl.LightningModule):
         # change second dimension to the last dimension
         # input1 = input1.transpose(1,2)
         # input2 = input2.transpose(1,2)
-
         output1 = self.model(input1) # hidden dimension only
         output2 = self.model(input2) # hidden dimension only
         output1 = output1.squeeze(0)
@@ -35,21 +34,23 @@ class SiameseNetTrainer(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         (x0, x1) , y = batch
         output1, output2 = self.forward(x0, x1)
+        # print("output1: ", output1, "output2: ", output2, "y: ", y)
         loss, predicted_labels = self.criterion(output1, output2, y.to(torch.float))
+        # predicted_labels = predicted_labels.squeeze(1)
         accuracy = torch.mean((predicted_labels == y.to(torch.float)).to(torch.float))
-        wandb.log({"train/loss": loss})
-        wandb.log({"train/epoch": self.current_epoch})
-        wandb.log({"train/accuracy": accuracy})
+        wandb.log({"epoch": self.current_epoch, "train/loss": loss})
+        wandb.log({"epoch": self.current_epoch, "train/accuracy": accuracy})
         return loss
 
     def validation_step(self, batch, batch_idx):
         (x0, x1) , y = batch
         output1, output2 = self.forward(x0, x1)
+        print("output1: ", output1, "output2: ", output2)
         loss, predicted_labels = self.criterion(output1, output2, y.to(torch.float))
+        # predicted_labels = predicted_labels.squeeze(1)
         accuracy = torch.mean((predicted_labels == y.to(torch.float)).to(torch.float))
-        wandb.log({"val/loss": loss})
-        wandb.log({"val/epoch": self.current_epoch})
-        wandb.log({"val/accuracy": accuracy})
+        wandb.log({"epoch": self.current_epoch, "val/loss": loss})
+        wandb.log({"epoch": self.current_epoch, "val/accuracy": accuracy})
         # predicted_labels , n_clusters_, homogeneity, completeness  = cluster_embeddings.cluster_embeddings_kmeans(2,output, y)
         # wandb.log({"val/n_clusters": n_clusters_})
         # wandb.log({"val/homogeneity": homogeneity})
@@ -61,9 +62,9 @@ class SiameseNetTrainer(pl.LightningModule):
         (x0, x1) , y = batch
         output1, output2 = self.forward(x0, x1)
         loss, predicted_labels = self.criterion(output1, output2, y.to(torch.float ))
+        # predicted_labels = predicted_labels.squeeze(1)
         accuracy = torch.mean((predicted_labels == y.to(torch.float)).to(torch.float))
         wandb.log({"test/loss": loss})
-        wandb.log({"test/epoch": self.current_epoch})
         wandb.log({"test/accuracy": accuracy})
         return loss
         

src/vqa/loss/ContrastiveSiameseLoss.py
@@ -9,8 +9,13 @@ class ContrastiveLoss(torch.nn.Module):
 
     def forward(self, output1, output2, label):
         euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-        predicted_labels = torch.where(euclidean_distance > self.margin, 1, 0)
+        # print("output1: ", output1.shape ,"\noutput2: ", output2.shape)
+        euclidean_distance = euclidean_distance.squeeze(1)
+        # print("euclidiean distance: ", euclidean_distance)
+        loss_contrastive = torch.mean(0.5 * (label) * torch.pow(euclidean_distance, 2) + 0.5 * (1-label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        # print("loss: ", loss_contrastive)
+        predicted_labels = torch.where(euclidean_distance > self.margin, 0, 1)
+        # print("predicted labels: ", predicted_labels)
         return loss_contrastive, predicted_labels
 
     